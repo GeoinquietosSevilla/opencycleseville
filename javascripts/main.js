5e4bb5ecde7696116bc42f1eec06e164e6ce54e2
Moisés Arcos <moiarcsan@gmail.com>
2014-07-03 20:53:37 +0200

    Changes to recolocate the popup correctly

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,7 @@
 function initMap() {
-	var map = L.map('map_results').setView([37.37, -5.963333], 11);
-	var MapQuestOpen_OSM = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpeg', {
-		attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-		subdomains: '1234'
+	var map = L.map('map_results').setView([37.37, -5.963333], 12);
+	var Thunderforest_OpenCycleMap = L.tileLayer('http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 	}).addTo(map);
 	var opl = new L.OverPassLayer({
 		query: 'http://overpass-api.de/api/interpreter?data=[out:json][timeout:25];(node["amenity"="bicycle_rental"](37.28880407455673,-6.1022186279296875,37.45496527884147,-5.844383239746094);way["amenity"="bicycle_rental"](37.28880407455673,-6.1022186279296875,37.45496527884147,-5.844383239746094);relation["amenity"="bicycle_rental"](37.28880407455673,-6.1022186279296875,37.45496527884147,-5.844383239746094););out body;>;out;',
@@ -19,8 +18,8 @@ function initMap() {
 	          var myicon = L.icon({
 	              iconUrl: 'images/cycling.png',
 	              iconSize: [32, 37],
-	              iconAnchor: [32, 37],
-	              popupAnchor: [-16, -35]
+	              iconAnchor: [16, 32],
+	              popupAnchor: [0, -30]
 	          });
 	          var marker = L.marker(pos, {icon: myicon}).bindPopup(popup);
 	          this.instance.addLayer(marker);
